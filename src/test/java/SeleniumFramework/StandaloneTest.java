0564e2720e82a1d985ed6a3a48a430dda97f350f
ssavelyeva1 <aliaksandra.savelyeva@gmail.com>
2023-04-06 13:02:35 +0200

    E2E test

--- src/test/java/SeleniumFramework/StandaloneTest.java
+++ src/test/java/SeleniumFramework/StandaloneTest.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -24,10 +26,22 @@ public class StandaloneTest {
 		driver.findElement(By.id("userPassword")).sendKeys("Abc_12345");
 		driver.findElement(By.id("login")).click();
 
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".mb-3")));
+
+		// finding Zara coat product and adding it to the cart
 		List<WebElement> productsList = driver.findElements(By.cssSelector(".mb-3"));
 		WebElement coatProduct = productsList.stream()
-				.filter(product -> product.findElement(By.cssSelector("b")).getText().equals("ZARA_COAT_3")).findFirst()
+				.filter(product -> product.findElement(By.cssSelector("b")).getText().equals("ZARA COAT 3")).findFirst()
 				.orElse(null);
+
+		coatProduct.findElement(By.cssSelector(".card-body button:last-of-type")).click();
+
+		// waiting till popup after adding the item to the cart appears
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#toast-container")));
+		wait.until(ExpectedConditions.invisibilityOf(driver.findElement(By.cssSelector(".ng-animating"))));
+
+		driver.findElement(By.cssSelector("[routerlink*='cart']")).click();
 
 	}
 
