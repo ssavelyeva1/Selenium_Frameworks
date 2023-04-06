89c7b3eab7f1a596a75373b4850f17a8f83d9cf1
ssavelyeva1 <aliaksandra.savelyeva@gmail.com>
2023-04-06 13:55:07 +0200

    e2e test 2

--- src/test/java/SeleniumFramework/StandaloneTest.java
+++ src/test/java/SeleniumFramework/StandaloneTest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -20,6 +21,7 @@ public class StandaloneTest {
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().window().maximize();
 
 		driver.get("https://rahulshettyacademy.com/client");
 
@@ -42,18 +44,29 @@ public class StandaloneTest {
 		// waiting till popup after adding the item to the cart appears
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#toast-container")));
 		wait.until(ExpectedConditions.invisibilityOf(driver.findElement(By.cssSelector(".ng-animating"))));
-		driver.findElement(By.cssSelector("[routerlink*='cart']")).click();
+		wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[routerlink*='cart']"))).click();
 
 		// making sure the item was added to the cart
 		List<WebElement> cartProducts = driver.findElements(By.cssSelector(".cartSection h3"));
-		Boolean match = cartProducts.stream().anyMatch(cartProduct->cartProduct.getText().equalsIgnoreCase(productName));
+		Boolean match = cartProducts.stream()
+				.anyMatch(cartProduct -> cartProduct.getText().equalsIgnoreCase(productName));
 		Assert.assertTrue(match);
-		
+
 		driver.findElement(By.cssSelector(".totalRow button")).click();
-		
+
+		// placing the order
+		Actions a = new Actions(driver);
+		a.sendKeys(driver.findElement(By.cssSelector("[placeholder='Select Country']")), "india").build().perform();
+
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".ta-results")));
+		driver.findElement(By.xpath("//button[contains(@class,'ta-item')][2]")).click();
+		driver.findElement(By.cssSelector(".action__submit")).click();
+
+		String confirmMessage = driver.findElement(By.cssSelector(".hero-primary")).getText();
+		Assert.assertTrue(confirmMessage.equalsIgnoreCase("THANKYOU FOR THE ORDER."));
+
+		driver.close();
+
 	}
 
 }
-
-//savelyeva1.20@gmail.com
-//Abc_12345
