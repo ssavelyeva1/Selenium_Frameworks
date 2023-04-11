06768d6f7376b18bb48964e8b86ca972b8cc1c34
ssavelyeva1 <aliaksandra.savelyeva@gmail.com>
2023-04-11 16:16:19 +0200

    multiple tests in one class

--- src/test/java/SeleniumFramework/tests/ErrorValidations.java
+++ src/test/java/SeleniumFramework/tests/ErrorValidations.java
@@ -1,20 +1,43 @@
 package SeleniumFramework.tests;
 
 import java.io.IOException;
+import java.util.List;
 
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import SeleniumFramework.pageobjects.CartPage;
+import SeleniumFramework.pageobjects.CheckoutPage;
+import SeleniumFramework.pageobjects.ConfirmationPage;
+import SeleniumFramework.pageobjects.ProductCatalog;
 import SeleniumFrameworkDesign.testcomponents.BaseTest;
 
 public class ErrorValidations extends BaseTest {
 
 	@Test
-	public void submitOrder() throws IOException {
+	public void LoginErrorValidation() throws IOException {
 		// logging to the web site with wrong credentials
 		landingPage.loginApplication("invanova1.20@gmail.com", "Def_6789");
 
 		Assert.assertEquals("Incorrect email or password.", landingPage.getErrorMessage());
+	}
+
+	@Test
+	public void ProductErrorValifation() throws IOException {
+		String productName = "ZARA COAT 3";
+
+		// logging to the web site
+		ProductCatalog productCatalog = landingPage.loginApplication("savelyeva1.20@gmail.com", "Abc_12345");
+
+		// finding and adding product to the cart
+		List<WebElement> products = productCatalog.getProductList();
+		productCatalog.addProductToCart(productName);
+		CartPage cartPage = productCatalog.goToCartPage();
+
+		// making sure the item was added to the cart and going to the checkout
+		Boolean match = cartPage.verifyProductDisplay("ZARA COAT 33");
+		Assert.assertFalse(match);
 	}
 
 }
