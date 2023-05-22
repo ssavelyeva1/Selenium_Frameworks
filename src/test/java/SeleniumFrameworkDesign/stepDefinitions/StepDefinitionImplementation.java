4df88d9789cb0c9ee823bfe4140881f67c284f65
ssavelyeva1 <aliaksandra.savelyeva@gmail.com>
2023-05-22 16:42:29 +0200

    step definition3

--- src/test/java/SeleniumFrameworkDesign/stepDefinitions/StepDefinitionImplementation.java
+++ src/test/java/SeleniumFrameworkDesign/stepDefinitions/StepDefinitionImplementation.java
@@ -4,17 +4,23 @@ import java.io.IOException;
 import java.util.List;
 
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 
+import SeleniumFramework.pageobjects.CartPage;
+import SeleniumFramework.pageobjects.CheckoutPage;
+import SeleniumFramework.pageobjects.ConfirmationPage;
 import SeleniumFramework.pageobjects.LandingPage;
 import SeleniumFramework.pageobjects.ProductCatalog;
 import SeleniumFrameworkDesign.testcomponents.BaseTest;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class StepDefinitionImplementation extends BaseTest{
 
 	public LandingPage landingPage;
 	public ProductCatalog productCatalog;
+	public ConfirmationPage confirmationPage;
 	
 	@Given("I landed on E-commerce page")
 	public void i_landed_on_E_commerce_page() throws IOException {
@@ -31,6 +37,24 @@ public class StepDefinitionImplementation extends BaseTest{
 	public void i_add_product_to_cart(String productName) {
 		List<WebElement> products = productCatalog.getProductList();
 		productCatalog.addProductToCart(productName);
+	}
+	
+	@When("^Checkout (.+) and submit the order$")
+	public void checkout_submit_order(String productName) {
+		CartPage cartPage = productCatalog.goToCartPage();
+
+		Boolean match = cartPage.verifyProductDisplay(productName);
+		Assert.assertTrue(match);
+		CheckoutPage checkoutPage = cartPage.goToCheckout();
+
+		checkoutPage.selectCountry("india");
+		confirmationPage = checkoutPage.submitOrder();
+	}
+	 
+	@Then("{string} message is displayed on ConfirmationPage")
+	public void message_displayed_confirmationPage(String string) {
+		String confirmMessage = confirmationPage.getConfirmationMessage();
+		Assert.assertTrue(confirmMessage.equalsIgnoreCase(string));
 	}
 	
 }
