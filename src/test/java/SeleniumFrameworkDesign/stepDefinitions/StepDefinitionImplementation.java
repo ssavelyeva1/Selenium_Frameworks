a8b34e1412316988cdb164ed9d0f62b27f799f98
ssavelyeva1 <aliaksandra.savelyeva@gmail.com>
2023-05-22 16:12:27 +0200

    step definition

---
+++ src/test/java/SeleniumFrameworkDesign/stepDefinitions/StepDefinitionImplementation.java
@@ -0,0 +1,12 @@
+package SeleniumFrameworkDesign.stepDefinitions;
+
+import io.cucumber.java.en.Given;
+
+public class StepDefinitionImplementation {
+
+	@Given("I landed on E-commerce page")
+	public void I_landed_on_E_commerce_page() {
+
+	}
+
+}
